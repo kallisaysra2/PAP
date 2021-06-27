cfc9d3ea36d595e12791f10a299d839bb066b095
kalyan ambati <73710239+kallisaysra2@users.noreply.github.com>
2021-06-28 04:40:04 +0530

    terraform files

---
+++ Provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+    features {}
+ subscription_id = "0101b00d-bb24-4848-8c6e-f214190140b2"
+ client_id = "d2a83f72-f775-4828-b7b0-ac09d9de199a"
+ client_secret = "ds1EJY-nQNla_iV8Dp-kA0Y5HiVizWsc.J"
+ tenant_id = "a45fe71a-f480-4e42-ad5e-aff33165aa35"
+}
---
+++ Variables.tf
@@ -0,0 +1,7 @@
+variable "TF" {
+  description = "The prefix used for all resources in this example"
+}
+
+variable "SouthIndia" {
+  description = "The Azure location where all resources in this example should be created"
+}
